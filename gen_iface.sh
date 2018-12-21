a8a4143b1dbdc148769ff06e56dc04d54fced8b4
AP1001 <Alejandro.Perez1001@protonmail.com>
2018-12-21 09:34:41 +0800

    add attributes to interface

--- gen_iface.sh
+++ gen_iface.sh
@@ -5,18 +5,18 @@ OUTPUT_F=./output/Makefile
 # Generate Makefile
 gen_iface(){
 cat >>${OUTPUT_F} <<EOL
-.PHONY: test_$1
-test_$1:
-	make -s -f basic.mk hint CONTENT="make $1"
+.PHONY: test_${1%% *}
+test_${1%% *}:
+	make -s -f basic.mk hint CONTENT="make ${1%% *}"
 	make -C \${TEST_PROJ} $1
 	make -C \${TEST_PROJ} status
 	make -s -f basic.mk confirm
 EOL
 }
 
-for i in ${iface[@]}
+for i in "${iface[@]}"
 do
-	gen_iface $i
+	gen_iface "$i"
 done
 
 
